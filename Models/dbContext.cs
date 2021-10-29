77a542829eb665821dc5267385855e492ff664cf
VICENTE NETO <vicente.viadana@portalsesisp.org.br>
2021-10-29 15:58:25 -0300

    29102021

--- Models/dbContext.cs
+++ Models/dbContext.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
 
 
 #nullable disable
@@ -35,11 +37,19 @@ namespace AcompanhamentoDocente.Models
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            if (!optionsBuilder.IsConfigured)
-            {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=localhost;Database=dbAcompanhamentoDocente;Trusted_Connection=True;");
-            }
+
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+            .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+            .AddJsonFile("appsettings.json")
+            .Build();
+            optionsBuilder.UseSqlServer(configuration.GetConnectionString("ConnectionStrings:AcompanhamentoDocente"));
+
+            //if (!optionsBuilder.IsConfigured)
+            //{
+#warning //To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                //optionsBuilder.UseSqlServer(@"Data Source=.\;Initial Catalog=dbAcompanhamentoDocente;Integrated Security=True");
+            //}
+            
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
